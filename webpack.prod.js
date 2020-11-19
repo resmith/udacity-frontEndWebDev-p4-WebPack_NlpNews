da0092713d2c603e2c4e8d99b43b61aca7a844c4
Robert Smith <txresmith@gmail.com>
2020-11-19 08:05:39 -0600

    Fixed issue with build-prod & css

--- webpack.prod.js
+++ webpack.prod.js
@@ -17,6 +17,11 @@ module.exports = {
         exclude: /node_modules/,
         loader: "babel-loader",
       },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: ["style-loader", "css-loader"],
+      },
     ],
   },
   plugins: [
